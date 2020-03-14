c907e863fac6295d684def7ba5c001ef0ef16ecb
Sergii Kogut <skogut@playtika.com>
2020-03-14 10:21:18 +0200

    modification

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-public class Main {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
